c8cc45868cd11450abfbb8212179a9636ee2c430
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-02-21 11:57:46 +0100

    Configure spring context

--- src/main/java/com/codecool/fileio/FileIoApplication.java
+++ src/main/java/com/codecool/fileio/FileIoApplication.java
@@ -1,12 +1,28 @@
 package com.codecool.fileio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class FileIoApplication {
+@ComponentScan("com.codecool.fileio")
+public class FileIoApplication implements CommandLineRunner{
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileIoApplication.class, args);
+    private ThreadsController threadsController;
+
+    public FileIoApplication(ThreadsController threadsController) {
+        this.threadsController = threadsController;
+    }
+
+    public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(FileIoApplication.class, args);
 	}
+
+    @Override
+    public void run(String... strings) throws Exception {
+        threadsController.toString();
+    }
 }
