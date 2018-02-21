2d278e9989de905f7252d225d2ddc2eba270bb42
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-02-21 13:23:17 +0100

    Create CopierImp class which implements Copier

--- src/main/java/com/codecool/fileio/CopierImp.java
+++ src/main/java/com/codecool/fileio/CopierImp.java
@@ -1,4 +1,15 @@
 package com.codecool.fileio;
 
-public class CopierImp {
+public class CopierImp implements Copier{
+
+
+    @Override
+    public void copy(String source, String destination) {
+
+    }
+
+    @Override
+    public int getProgress() {
+        return 0;
+    }
 }
