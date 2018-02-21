ab3caf1bd07a940f124effd4c51422236089c210
Szymon Wojtas <szymon.wojtas@gmail.com>
2018-02-21 14:08:03 +0100

    Change getProgress() method to return ProgressDisplayer object

--- src/main/java/com/codecool/fileio/Copier.java
+++ src/main/java/com/codecool/fileio/Copier.java
@@ -1,7 +1,10 @@
 package com.codecool.fileio;
 
+import java.io.IOException;
+
 public interface Copier {
 
-    public void copy(String source, String destination);
-    public int getProgress();
+    public void copy(String source, String destination) throws IOException;
+    public void stop();
+    public ProgressDisplayer getProgress();
 }
